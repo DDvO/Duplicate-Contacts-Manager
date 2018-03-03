6f77e93b96f011f9db02191d1503ab2087ac9aa9
David von Oheimb <dev@ddvo.net>
2018-03-03 12:07:14 +0100

    version 1.0.1 (new number required for updating submission on addons.mozilla.org)

--- zip.sh
+++ zip.sh
@@ -1,2 +1,2 @@
 #cd duplicatecontactsmanager@ddvo.net
-zip -r ../duplicatecontactsmanager-1.0.xpi . -x \*~ .git/\* .git README.md zip.sh
+zip -r ../duplicatecontactsmanager-1.0.xpi . -x \*~ .git/\* .git README.md zip.sh .gitignore
